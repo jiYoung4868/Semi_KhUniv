682e3da12a6b93cf3d668354eeee30902788a217
jiYoung4868 <orinogcode@gmail.com>
2023-08-03 10:47:55 +0900

    Login 기능 기본 세팅 시작
    - login servlet
    - jdbc template

---
+++ src/main/java/common/JdbcTemplate.java
@@ -0,0 +1,96 @@
+
+package common;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JdbcTemplate {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnectionKh() {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","kh","kh");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if(conn!=null) {
+			System.out.println("[jy] DB 연결 성공");
+		}else {
+			System.out.println("[jy] DB 연결 실패");
+		}
+		return conn;
+	}
+	
+	public static void close(Connection conn) {
+		try {
+			if(conn!=null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void close(Statement stmt) {
+		try {
+			if(stmt!=null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void close(ResultSet rset) {
+		try {
+			if(rset!=null) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void setAutoCommit(Connection conn, boolean auto) {
+		try {
+			conn.setAutoCommit(auto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void commit(Connection conn) {
+		try {
+			conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void rollback(Connection conn) {
+		try {
+			conn.rollback();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+}
