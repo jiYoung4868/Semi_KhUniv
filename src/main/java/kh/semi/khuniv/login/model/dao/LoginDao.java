d0c93ecade6f3458c7fddea308ff74d189ceba49
jiYoung4868 <orinogcode@gmail.com>
2023-08-18 10:29:12 +0900

    login mybatis 오류문제 해결
    config에 mapper등록 안함

--- src/main/java/kh/semi/khuniv/login/model/dao/LoginDao.java
+++ src/main/java/kh/semi/khuniv/login/model/dao/LoginDao.java
@@ -15,9 +15,8 @@ import static kh.semi.khuniv.common.jdbc.JdbcTemplate.*;
 public class LoginDao {
 
 	public LoginVo sLogin(SqlSession session, StudentVo vo) {
-		System.out.println("[jy] LoginDao.sLogin.studentVo:" + vo);
+		System.out.println("[jy] LoginDao.sLogin vo.:" + vo);
 		String studentName= session.selectOne("khUniv.studentLogin", vo);
-		System.out.println(studentName);
 		LoginVo result = new LoginVo(vo.getStudentNo(), studentName, 1);
 		System.out.println("[jy] LoginDao.sLogin.return: " + result);
 		return result;
@@ -25,7 +24,8 @@ public class LoginDao {
 
 	public LoginVo pLogin(SqlSession session, ProfessorVo vo) {
 		System.out.println("[jy] LoginDao.pLogin vo:" + vo);
-		LoginVo result= session.selectOne("khUniv.professorLogin", vo);
+		String professorName= session.selectOne("khUniv.professorLogin", vo);
+		LoginVo result = new LoginVo(vo.getProfessorNo(), professorName, 1);
 		System.out.println("[jy] LoginDao.pLogin return: " + result);
 		return result;
 	}
